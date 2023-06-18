d5cc9807e4c41ea7af61788ce1374485ff0877df
= <=>
2023-06-18 15:49:48 +0200

    Scaling z axis aspect ratio

--- CRNpp/src/Rendering/Plot.fs
+++ CRNpp/src/Rendering/Plot.fs
@@ -5,18 +5,33 @@ namespace Rendering
 module Plotting =
     open Plotly.NET
     open Plotly.NET.TraceObjects
+    open Plotly.NET.LayoutObjects
+    open Plotly.NET.StyleParam
     type Plot = P of GenericChart.GenericChart
 
-    type DataPoints = float list
-    let scatter (x:DataPoints) (y:DataPoints) label= 
-        Chart.Scatter(X=x, Y=y, Mode=StyleParam.Mode.Lines_Markers, Name=label)
+    let scatter x y label= 
+        Chart.Scatter(X=x, Y=y, Mode=Mode.Lines_Markers, Name=label)
         |> P
 
-    let surface (x:DataPoints) (y:DataPoints) (z:DataPoints list) xLabel yLabel zLabel =
-        Chart.Surface(X=x, Y=y, zData=z, Contours=Contours.initXyz(Show=true), ShowScale=false)
-        |> Chart.withXAxisStyle (xLabel, ShowGrid=false, ShowLine=false)
-        |> Chart.withYAxisStyle (yLabel, ShowGrid=false, ShowLine=false)
-        |> Chart.withZAxisStyle (zLabel, ShowGrid=false, ShowLine=false)
+    let surface x y z xLabel yLabel zLabel =
+        let axis title scaleRatio scaleAnchor id = LinearAxis.init (
+                Title=Title.init title, 
+                ScaleRatio=scaleRatio, 
+                ScaleAnchor=scaleAnchor,
+                Overlaying = id
+            )
+
+        let free = LinearAxisId.Free
+        let xId = LinearAxisId.X 0
+        let yId = LinearAxisId.Y 0
+
+        let xAxis = axis xLabel 1.0 free xId
+        let yAxis = axis yLabel 1.0 free yId
+        let zAxis = axis zLabel 0.5 xId free
+
+        Chart.Surface (X=x, Y=y, zData=z, Contours=Contours.initXyz(Show=true), ShowScale=false)
+        |> Chart.withSceneStyle (XAxis=xAxis, YAxis=yAxis, ZAxis=zAxis, 
+                                AspectRatio=AspectRatio.init(1.0, 1.0, 0.5), AspectMode=AspectMode.Manual)
         |> P
 
     let showPlot title (P(chart)) =
